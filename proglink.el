5debf1e3254aa7bd3f89608b05205ed6365dab17
zyt-zsn <zyt_xl@163.com>
2024-07-14 16:05:37 +0800

    Create a new line to insert link when current line is not empyt.

--- proglink.el
+++ proglink.el
@@ -21,6 +21,13 @@
 	  )
 	)
   )
+
+;; https://emacs.stackexchange.com/questions/16792/easiest-way-to-check-if-current-line-is-empty-ignoring-whitespace
+(defun current-line-empty-p ()
+  (save-excursion
+    (beginning-of-line)
+    (looking-at-p "[[:blank:]]*$")))
+
 (defun zyt/prog-insert-link()
   (interactive)
   (let* ((cur-buff (current-buffer))
@@ -52,9 +59,14 @@
 							)
 						  )
 						))
-
 					)))
 	(with-current-buffer cur-buff
+	  (unless (current-line-empty-p)
+		(if (featurep 'evil)
+			(evil-open-below 1)
+		  (end-of-line)
+		  (newline))
+		)
 	  (insert (format "[[**  (bookmark--jump-via \"%s\" 'switch-to-buffer-other-window)  **]]" bookmark))
 	  (comment-line 1)
 	  ;; [[**  (bookmark-jump "org link font lock")  **]]
@@ -62,13 +74,13 @@
 	  (set-text-properties
 	   (pos-bol) (pos-eol)
 	   '(
-		   mouse-face highlight
-		   help-echo "mouse-2: visit this file in other window"
-		   font-lock-fontified nil
-		   ;; face org-document-title
-		   ;; face tdr-font-mode
-		   face org-link
-		   ))
+		 mouse-face highlight
+		 help-echo "mouse-2: visit this file in other window"
+		 font-lock-fontified nil
+		 ;; face org-document-title
+		 ;; face tdr-font-mode
+		 face org-link
+		 ))
 	  )
 	))
 ;; [[**  (bookmark--jump-via "SCSI_Peripheral_Device_Type" 'switch-to-buffer-other-window)  **]]
@@ -77,39 +89,39 @@
 
 
 
- (define-key prog-mode-map (kbd "C-c C-l") #'zyt/prog-insert-link)
+(define-key prog-mode-map (kbd "C-c C-l") #'zyt/prog-insert-link)
 ;; c-mode-base-map 默认将 ~C-c C-l~绑定在了 ~c-toggle-electric-state~
- (define-key c-mode-base-map (kbd "C-c C-l") #'zyt/prog-insert-link)
+(define-key c-mode-base-map (kbd "C-c C-l") #'zyt/prog-insert-link)
 
 (defun zyt()
   (interactive)
   ;; (let ((bookmark-name
-		 ;; (ido-completing-read
-		 (ivy-read
-		  "插入标签: "
-		  (--map 
-		   (substring-no-properties (bookmark-name-from-full-record it))
-		   (bookmark-maybe-sort-alist))
-		  :action
-		  #'action
-		  ;; (lambda(bookmark)
-		  ;; 	(bookmark--jump-via bookmark 'switch-to-buffer-other-window)
-		  ;; 	)
-		  :update-fn
-		  (lambda ()
-			(let ((cur-buf(current-buffer)))
-			  (progn
-				(save-excursion
-				(bookmark--jump-via  (ivy-state-current ivy-last) 'switch-to-buffer-other-window)
-				(pop-to-buffer cur-buf '(display-buffer-in-previous-window . ()) t)
-				)
-				(message (format "content of bookmark %s" (ivy-state-current ivy-last)))
-				)
-			  )
-			)))
-		;; (message (format "Hello %s\n" bookmark-name))
-		;; (bookmark--jump-via bookmark-name 'switch-to-buffer-other-window)
-		;; )))
+  ;; (ido-completing-read
+  (ivy-read
+   "插入标签: "
+   (--map 
+	(substring-no-properties (bookmark-name-from-full-record it))
+	(bookmark-maybe-sort-alist))
+   :action
+   #'action
+   ;; (lambda(bookmark)
+   ;; 	(bookmark--jump-via bookmark 'switch-to-buffer-other-window)
+   ;; 	)
+   :update-fn
+   (lambda ()
+	 (let ((cur-buf(current-buffer)))
+	   (progn
+		 (save-excursion
+		   (bookmark--jump-via  (ivy-state-current ivy-last) 'switch-to-buffer-other-window)
+		   (pop-to-buffer cur-buf '(display-buffer-in-previous-window . ()) t)
+		   )
+		 (message (format "content of bookmark %s" (ivy-state-current ivy-last)))
+		 )
+	   )
+	 )))
+;; (message (format "Hello %s\n" bookmark-name))
+;; (bookmark--jump-via bookmark-name 'switch-to-buffer-other-window)
+;; )))
 
 (defvar zyt/prog-link-mode-map
   (let ((map (make-sparse-keymap)))
